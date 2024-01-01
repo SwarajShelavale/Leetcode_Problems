3987683eb0b9184a1bb2afc5fc87c6ed493b011e
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-01-02 00:34:21 +0530

    Time: 10 ms (13.48%) | Memory: 45.5 MB (6.13%) - LeetSync

---
+++ 455-assign-cookies/assign-cookies.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int count=0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0,j=0;
+       while(i<s.length)
+        {
+            while(j<g.length)
+            {
+                if(g[j]<=s[i])
+                {
+                    count++;
+                    j++;
+                    break;
+                }
+                else
+                { 
+                    break;
+                }
+            }
+            i++;
+        }
+
+        return count;
+    }
+}
