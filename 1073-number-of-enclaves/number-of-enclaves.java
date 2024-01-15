08ab1d15f171c2186c67b1d3806cd84d216ff0c2
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-01-16 04:26:54 +0530

    Time: 6 ms (98.62%) | Memory: 56.6 MB (42.80%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -5,27 +5,21 @@ class Solution {
         //Travel Corners
         for(int i=0;i<n;i++)
         {
-            if(board[i][0]==1)
-                dfs(i,0,board);
-
-             if(board[i][m-1]==1)
-                dfs(i,m-1,board);
+            dfs(i,0,board);
+            dfs(i,m-1,board);
         }
 
         for(int j=0;j<m;j++)
         {
-            if(board[0][j]==1)
-                dfs(0,j,board);
-
-             if(board[n-1][j]==1)
-                dfs(n-1,j,board);
+            dfs(0,j,board);
+            dfs(n-1,j,board);
         }
 
 
         int count=0;
-        for(int i=0;i<n;i++)
+        for(int i=1;i<n-1;i++)
         {
-            for(int j=0;j<m;j++)
+            for(int j=1;j<m-1;j++)
             {
                 if(board[i][j]==1)
                     count++;
