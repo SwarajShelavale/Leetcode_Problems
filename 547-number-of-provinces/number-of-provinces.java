762703bb8f896cbb9c91c0dfa76537e01a8b4702
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-01-15 16:36:14 +0530

    Time: 0 ms (100.00%) | Memory: 47.7 MB (24.40%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,21 +1,21 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int N=isConnected.length;
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
-        for(int i=0;i<N;i++)
-            adj.add(new ArrayList<Integer>());
+        // ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        // for(int i=0;i<N;i++)
+        //     adj.add(new ArrayList<Integer>());
         
-        for(int i=0;i<N;i++)
-        {
-            for(int j=0;j<N;j++)
-            {
-                if(i!=j && isConnected[i][j]==1)
-                {
-                    adj.get(i).add(j);
-                    adj.get(j).add(i);
-                }
-            }
-        }
+        // for(int i=0;i<N;i++)
+        // {
+        //     for(int j=0;j<N;j++)
+        //     {
+        //         if(i!=j && isConnected[i][j]==1)
+        //         {
+        //             adj.get(i).add(j);
+        //             adj.get(j).add(i);
+        //         }
+        //     }
+        // }
 
         int count = 0;
         boolean visited[] =  new boolean[N];
@@ -25,20 +25,33 @@ class Solution {
             if(!visited[i])
             {
                 count++;
-                dfs(i,visited,adj);
+                // dfs(i,visited,adj);
+                dfs(i,visited,isConnected);
             }
         }
 
         return count;
     }
 
-    void dfs(int node,boolean visited[] , ArrayList<ArrayList<Integer>> adj )
+    // void dfs(int node,boolean visited[] , ArrayList<ArrayList<Integer>> adj )
+    // {
+    //     visited[node] = true;
+    //     for(int x : adj.get(node))
+    //     {
+    //         if(!visited[x])
+    //             dfs(x,visited,adj);
+    //     }
+    // }
+
+    void dfs(int i,boolean visited[] ,int[][] isConnected)
     {
-        visited[node] = true;
-        for(int x : adj.get(node))
+        visited[i] = true;
+        for(int j=0;j<isConnected.length;j++)
         {
-            if(!visited[x])
-                dfs(x,visited,adj);
+            if(i!=j && !visited[j] && isConnected[i][j]==1)
+            {
+                    dfs(j,visited,isConnected);
+            } 
         }
     }
 }
