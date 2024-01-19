2e54ec61cf5ba5010d69513fd49c7b050f66a080
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-01-20 03:17:07 +0530

    Time: 6 ms (68.87%) | Memory: 44.5 MB (79.77%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -6,16 +6,19 @@ class Solution {
         {
             for(int j=0;j<n;j++)
             {
-                if(j==0)
+                // handled separate corner case for first(j==0) && last(n-1) column
+                if(j==0)  
                     matrix[i][j]+=Math.min(matrix[i-1][j],matrix[i-1][j+1]);
-                else if(j==n-1)
-                    matrix[i][j]+=Math.min(matrix[i-1][j],matrix[i-1][j-1]);
-                else
+                else if(j==n-1)            // prev middle   prev right
+                    matrix[i][j]+=Math.min(matrix[i-1][j-1],matrix[i-1][j]);
+                else                     //  prev left    prev middle
                     matrix[i][j]+=Math.min(matrix[i-1][j],Math.min(matrix[i-1][j-1],matrix[i-1][j+1]));
-
+                                            //prev left             prev middle      prev right
             }
         }
 
+        //So now last row will have all the possible answers of pathSum
+        //So we will find minOfall last row Cells
         int minSum=matrix[n-1][0];
         for(int i=1;i<n;i++)
             minSum=Math.min(minSum,matrix[n-1][i]);
@@ -23,3 +26,5 @@ class Solution {
         return minSum;
     }
 }
+
+
