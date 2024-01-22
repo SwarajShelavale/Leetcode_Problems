0d5b221b21b7f21aef5673fc7aa13ef559d50fa0
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-01-22 20:40:28 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (36.28%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,8 +1,10 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m=obstacleGrid.length,n=obstacleGrid[0].length;
+
         if(obstacleGrid[0][0]==1 || obstacleGrid[m-1][n-1]==1)
             return 0;
+
         int dp[][] = new int[m][n];
         dp[0][0]=1;
         for(int i=1;i<n;i++)
@@ -20,15 +22,17 @@ class Solution {
             else
                 dp[i][0]=1;
         }
-        
+
         for(int i=1;i<m;i++)
         {
             for(int j=1;j<n;j++)
             {
                 if(obstacleGrid[i][j]==1)
                     dp[i][j]=0;
+                else if(i==0 || j==0)
+                    dp[i][j]=1;
                 else
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];                
             }
         }
 
