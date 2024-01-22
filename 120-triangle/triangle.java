cc51ee6cc9729eeb108eae085279ae569ffd5f1e
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-01-23 03:59:17 +0530

    Time: 8 ms (18.55%) | Memory: 44.2 MB (44.40%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,31 +1,36 @@
+import java.util.List;
+
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n=triangle.size();
+        int n = triangle.size();
 
-        for(int i=1;i<n;i++)
-        {
-           int m=triangle.get(i).size();
-           for(int j=0;j<m;j++)
-           {
-               if(j==0)
-               {
-                   triangle.get(i).set(j,triangle.get(i).get(j)+triangle.get(i-1).get(j));
-               }
-               else if(j<m-1)
-               {
-                   triangle.get(i).set(j,triangle.get(i).get(j)+Math.min(triangle.get(i-1).get(j-1),triangle.get(i-1).get(j)));
-               }
-               else
-               {
-                    triangle.get(i).set(j,triangle.get(i).get(j)+triangle.get(i-1).get(j-1));
-               }
-           }
+        if (n == 1)
+            return triangle.get(0).get(0);
+
+        // Create a separate list to store the minimum path sums
+        List<List<Integer>> minPathSums = new ArrayList<>(triangle);
+
+        for (int i = 1; i < n; i++) {
+            int m = triangle.get(i).size();
+
+            for (int j = 0; j < m; j++) {
+                int val = triangle.get(i).get(j);
+
+                if (j == 0)
+                    minPathSums.get(i).set(j, minPathSums.get(i - 1).get(j) + val);
+                else if (j == m - 1)
+                    minPathSums.get(i).set(j, minPathSums.get(i - 1).get(j - 1) + val);
+                else
+                    minPathSums.get(i).set(j, Math.min(minPathSums.get(i - 1).get(j), minPathSums.get(i - 1).get(j - 1)) + val);
+            }
         }
-        int minPathSum=Integer.MAX_VALUE;
-        for(int j=0;j<triangle.get(n-1).size();j++)
-        {
-            minPathSum=Math.min(minPathSum,triangle.get(n-1).get(j));
-        }
-    return minPathSum;
+
+        int mini = Integer.MAX_VALUE;
+
+        // Find the minimum path sum in the last row of minPathSums
+        for (int j = 0; j < minPathSums.get(n - 1).size(); j++)
+            mini = Math.min(mini, minPathSums.get(n - 1).get(j));
+
+        return mini;
     }
 }
