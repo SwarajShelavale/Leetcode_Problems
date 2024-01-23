8c0219f34687a40083e58dbdb45f2941dd4fdd29
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-01-23 19:03:15 +0530

    Time: 17 ms (54.59%) | Memory: 45 MB (19.57%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int dp[][] = new int[n+1][amount+1];
+        int maxValue = Integer.MAX_VALUE-1;
+        
+        for(int i=0;i<=n;i++)
+            dp[i][0] = 0 ;
+        
+        for(int j=0;j<=amount;j++)
+        {
+            if((j%coins[0])==0)
+                dp[1][j] =j/coins[0]; 
+            else
+                dp[1][j]=maxValue;
+        }
+
+        for(int i=2;i<=n;i++)
+        {
+            for(int j=1;j<=amount;j++)
+            {
+               if (coins[i-1] <= j) {
+                    dp[i][j] = Math.min(dp[i-1][j], 1 + dp[i][j - coins[i-1]]);
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+
+        return dp[n][amount]==maxValue ? -1:dp[n][amount];
+    }
+}
