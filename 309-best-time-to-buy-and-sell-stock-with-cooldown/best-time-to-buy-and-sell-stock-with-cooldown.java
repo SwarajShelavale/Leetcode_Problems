70105b69116c23113af1ef5228d56f3a4f4d8883
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-01-25 02:30:45 +0530

    Time: 1 ms (84.08%) | Memory: 41.5 MB (49.28%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,37 +1,25 @@
 class Solution {
-    //States:
-    //      0 - Buy
-    //      1 - Sell
-    //      2 - CoolDown
-
-    private int dp[][];
     public int maxProfit(int[] prices) {
-        
-        dp = new int[prices.length][3];
-        for(int i=0;i<prices.length;i++)
-            Arrays.fill(dp[i],-1);
-        return profit(0,prices,2);
-    }
+        int n=prices.length;
 
-    int profit(int index,int[] prices,int current_state)
-    {
-        if(index==prices.length)
-            return 0;
-        
-        if(dp[index][current_state]!=-1)
-            return dp[index][current_state];
+        int dp[][] = new int[n+1][3];
+        //0-can buy
+        //1-can sell
+        //2-can cooldown
+        for (int i = n-1; i >=0; i--) {
+            int toBuy = -prices[i] + dp[i + 1][1];
+            int notToBuy = dp[i+1][0];
 
-        if(current_state==2) //current_State is cooldown
-        {
-            return dp[index][current_state]=Math.max(-prices[index]+profit(index+1,prices,0),profit(index+1,prices,2));
+            int toSell = prices[i] + dp[i+1][2];
+            int notToSell = dp[i+1][1];
+
+            int toCoolDown = dp[i+1][0];
+
+            dp[i][0] = Math.max(Math.max(toBuy, notToBuy),toCoolDown);
+            dp[i][1] = Math.max(toSell, notToSell);
+            dp[i][2] = toCoolDown;
         }
-        else if(current_state==1) //current_State is sell
-        {
-            return dp[index][current_state]=profit(index+1,prices,2);
-        }
-        else //current_State is buy
-        {
-            return dp[index][current_state]=Math.max(prices[index]+profit(index+1,prices,1),profit(index+1,prices,0));
-        }
+
+        return dp[0][0];
     }
 }
