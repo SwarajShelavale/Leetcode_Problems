9dfc1e01209a088a3b9c2b08bef57000834ded96
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-01-29 16:49:33 +0530

    Time: 8 ms (18.63%) | Memory: 47 MB (9.43%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -2,8 +2,21 @@ class Solution {
     public int[] countBits(int n) {
         int res[] = new int[n+1];
         for(int i=0;i<=n;i++)
-            res[i] = Integer.bitCount(i);
+        {
+            res[i] = countSetBits(i);
+        }
         
         return res;
+    }
+
+    int countSetBits(int n)
+    {
+        int count=0;
+        for(int i=0;i<31;i++)
+        {
+                count+= (n&1)==1 ? 1:0;
+            n=n>>1;
+        }
+        return count;
     }
 }
