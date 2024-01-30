68077d252b53418c7241eff32f08d0f72ea116b3
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-01-30 19:31:11 +0530

    Time: 2 ms (88.51%) | Memory: 47.6 MB (68.50%) - LeetSync

---
+++ 143-reorder-list/reorder-list.java
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        
+        ListNode slow = head,fast = head;
+
+        while(fast!=null && fast.next!=null)
+        {
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        ListNode head2=slow.next;
+        slow.next=null;
+
+        head2 = reverseList(head2);
+
+        ListNode cur1=head,cur2=head2,temp1,temp2;
+        while(cur1!=null && cur2!=null)
+        {
+            temp1=cur1.next;
+            temp2=cur2.next;
+            cur1.next=cur2;
+            cur2.next=temp1;
+            cur2=temp2;
+            cur1=temp1;
+        }
+    }
+
+    ListNode reverseList(ListNode  head)
+    {
+        ListNode prev=null;
+        ListNode cur=head,temp;
+
+        while(cur!=null)
+        {
+            temp=cur.next;
+            cur.next=prev;
+            prev=cur;
+            cur=temp;
+        }
+
+        return prev;
+    }
+}
+
+// 1 2 3 4
+// 1-> 4 -> 2 -> 3
+
+
+// 1 2 3 4 5
+
+// 1 5 2 4 3
