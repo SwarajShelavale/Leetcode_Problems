0b447bd89f6ada5eac8ccb7663e6e0c3d4f76ac9
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-02-01 20:16:40 +0530

    Time: 7 ms (20.11%) | Memory: 49.3 MB (61.27%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,84 +1,52 @@
 class Solution {
+    class Pair {
+        int first;
+        int second;
+
+        public Pair(int first, int second) {
+            this.first = first;
+            this.second = second;
+        }
+    }
+
     public int numIslands(char[][] grid) {
-        int n = grid.length , m = grid[0].length ;
+        int count = 0;
+        int n = grid.length, m = grid[0].length;
+        boolean[][] visited = new boolean[n][m];
 
-        UnionFind uf  = new UnionFind(n*m);
-
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]=='1')
-                {
-                    if(i>0 && grid[i-1][j]=='1')
-                        uf.unionByRank(i*m+j, (i-1)*m+j);
-                    
-                    if(j>0 && grid[i][j-1]=='1')
-                        uf.unionByRank(i*m+j, i*m+(j-1));
-                    
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j] && grid[i][j] == '1') {
+                    bfs(i, j, grid, visited);
+                    count++;
                 }
             }
         }
+        return count;
+    }
 
-        HashSet<Integer> hs = new HashSet<>();
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(grid[i][j]=='1')
-                    hs.add(uf.findParent(i*m+j));
+    void bfs(int i, int j, char[][] grid, boolean[][] visited) {
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(i, j));
+        visited[i][j] = true;
+        int n = grid.length, m = grid[0].length;
+
+        while (!q.isEmpty()) {
+            int row = q.peek().first;
+            int col = q.peek().second;
+            q.poll();
+
+            int[][] directions = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
+
+            for (int[] dir : directions) {
+                int nRow = row + dir[0];
+                int nCol = col + dir[1];
+
+                if (nRow >= 0 && nCol >= 0 && nRow < n && nCol < m && !visited[nRow][nCol] && grid[nRow][nCol] == '1') {
+                    visited[nRow][nCol] = true;
+                    q.add(new Pair(nRow, nCol));
+                }
             }
         }
-
-        return hs.size();
-
     }
-}
-
-
-class UnionFind {
-
-    int parent[];
-    int rank[];
-
-    UnionFind(int size)
-    {
-        parent  = new int[size];
-        rank    = new int[size];
-
-        for(int i=0;i<size;i++)
-        {
-            parent[i]=i;
-            rank[i]=1;
-        }
-    }
-
-    public void unionByRank(int u,int v)
-    {
-        int ulp_u = findParent(u);
-        int ulp_v = findParent(v);
-
-        if(ulp_u==ulp_v)
-            return;
-        
-        if(rank[ulp_u] < rank[ulp_v])
-            parent[ulp_u]=ulp_v;
-        else if(rank[ulp_v] < rank[ulp_u])
-            parent[ulp_v]=ulp_u;
-        else 
-        {
-            parent[ulp_v]=ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-
-    public int findParent(int u)
-    {
-        if(parent[u]==u)
-            return u;
-        
-        return parent[u] = findParent(parent[u]);
-    }
-
-
 }
