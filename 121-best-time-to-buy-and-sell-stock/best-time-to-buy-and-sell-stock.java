b79070967c17ba9346242799933b3cfddd0438e4
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-02-01 18:43:34 +0530

    Time: 2 ms (78.73%) | Memory: 61.7 MB (45.99%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+       int profit=0,i=0,j=1;
+        while(j<prices.length)
+        {
+            if(prices[i]<prices[j])
+                profit=Math.max(profit,prices[j]-prices[i]);
+            else
+                i=j;
+            j++;
+        }
+        return profit;
+    }
+}
+
+        
+       
