32a6ea652ee0aec3e4ab2c136129557417916af8
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-02-01 21:37:57 +0530

    Time: 134 ms (25.62%) | Memory: 63.2 MB (93.89%) - LeetSync

---
+++ 295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -0,0 +1,29 @@
+import java.util.PriorityQueue;
+
+class MedianFinder {
+
+    private PriorityQueue<Integer> maxHeap;
+    private PriorityQueue<Integer> minHeap;
+
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>((a, b) -> b - a);
+        minHeap = new PriorityQueue<>((a, b) -> a - b);
+    }
+
+    public void addNum(int num) {
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+
+        if (maxHeap.size() < minHeap.size()) {
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        } else {
+            return maxHeap.peek();
+        }
+    }
+}
