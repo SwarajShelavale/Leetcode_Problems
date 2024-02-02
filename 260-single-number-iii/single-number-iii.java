38c7867997f9211eb884b52f5d5a1b318084725d
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-02-02 17:25:51 +0530

    Time: 1 ms (99.84%) | Memory: 45.6 MB (38.31%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,22 +1,19 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        int singleNumber=0;
-        for(int i=0;i<nums.length;i++)
-            singleNumber^=nums[i];
+        int XOR = 0;
+        for (int i = 0; i < nums.length; i++)
+            XOR ^= nums[i];
 
-        singleNumber = singleNumber & ~(singleNumber-1); //Find the lowest set bit
+        XOR = XOR & ~(XOR - 1); // Find the lowest set bit
 
-        int a=0,b=0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if((singleNumber & nums[i])!=0)
-                a^=nums[i];
+        int a = 0, b = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if ((XOR & nums[i]) != 0)
+                a ^= nums[i];
             else
-                b^=nums[i];
-
+                b ^= nums[i];
         }
-       
-        return new int[]{a,b};
 
+        return new int[]{a, b};
     }
 }
