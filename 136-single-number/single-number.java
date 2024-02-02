85a6d4f7ff88ff7021ca7d468d06b9afe98ddf5f
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-02-02 16:58:41 +0530

    Time: 1 ms (99.89%) | Memory: 46 MB (40.65%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int singleNumber = 0;
+        for(int n:nums)
+            singleNumber ^= n;
+
+        return singleNumber;  
+    }
+}
