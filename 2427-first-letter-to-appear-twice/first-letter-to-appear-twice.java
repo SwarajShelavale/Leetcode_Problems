f9712ef42f6221b75fa5f8748eb61b05129624a6
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2024-02-06 01:58:34 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (43.20%) - LeetSync

---
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char repeatedCharacter(String s) {
+         HashSet<Character> hs = new HashSet<>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(hs.contains(s.charAt(i)))
+                return s.charAt(i);
+            else
+                 hs.add(s.charAt(i));
+        }
+        return s.charAt(0);
+    }
+}
