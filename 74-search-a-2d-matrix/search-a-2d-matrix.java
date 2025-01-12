e251e98102836ff5739201ab3d2ca23b778e53e8
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2025-01-12 20:36:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (61.10%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,14 +1,44 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        for(int[] mat: matrix){
-            for(int value : mat)
+        
+        //Approach 1: Brute Force / Naive
+        // for(int[] mat: matrix){
+        //     for(int value : mat)
+        //     {
+        //         if(value == target)
+        //         {
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        //Approach 2: Efficient
+        int n = matrix.length , m= matrix[0].length;
+        for(int i=0; i< n;i++)
+        {
+            if( target>= matrix[i][0] && target <=matrix[i][m-1])
             {
-                if(value == target)
+                for(int j=0;j<m;j++)
                 {
-                    return true;
+                    if(matrix[i][j]==target)
+                        return true;
                 }
+                return false;
             }
         }
         return false;
     }
+
 }
+
+// target = 23
+// 1 3 5 7
+// 10 11 16 20
+// 23 30 34 60
+
+// 100 * 100
+
+// operations -> 10000
+
+// 100 + 100 = 200
