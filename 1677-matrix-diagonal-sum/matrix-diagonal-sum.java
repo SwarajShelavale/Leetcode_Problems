ee32c78778f1e2a4e9a23eea5bfaceb12abe02f5
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2025-01-12 20:13:35 +0530

    Time: 1 ms (29.50%) | Memory: 44.7 MB (12.84%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -4,14 +4,17 @@ class Solution {
         int row = mat.length, col= mat[0].length;
         for(int i=0;i<row;i++)
         {
-            sum += (mat[i][i]) + (mat[i][col-i-1]);
-            // for(int j=0;j<col;j++)
-            // {                           
-            //     if(i==j)  // TopLeft---> BottomRight
-            //         sum+= mat[i][j];
-            //     if(j == col-i-1)  // TopRight --> BottomLeft
-            //         sum+= mat[i][j];
-            // }  
+           //Approach 1: Brute-Force / Naive
+            for(int j=0;j<col;j++)
+            {                           
+                if(i==j)  // TopLeft---> BottomRight
+                    sum+= mat[i][j];
+                if(j == col-i-1)  // TopRight --> BottomLeft
+                    sum+= mat[i][j];
+            }  
+
+            //Approach 2: Efficient
+            //  sum += (mat[i][i]) + (mat[i][col-i-1]);
         }
 
         if(row%2==1)
@@ -23,4 +26,3 @@ class Solution {
     }
 }
 
-//  sum+= (mat[i][i] +  mat[i][n-i-1]);
