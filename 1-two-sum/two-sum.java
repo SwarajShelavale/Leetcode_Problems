b343688aa43c2a50927f78b8a62a9f78b79a2544
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2025-01-19 22:50:00 +0530

    Time: 2 ms (98.92%) | Memory: 44.9 MB (73.35%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,23 +1,53 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       int  sum = 0;
+    //    int  sum = 0;
 
-        for(int i=0;i<nums.length;i++)
+    //     for(int i=0;i<nums.length;i++)
+    //     {
+    //         for(int j=i+1;j<nums.length;j++)
+    //         {
+    //             if(nums[i]+nums[j] == target)
+    //             { 
+    //                 return new int[]{i,j};
+    //             }       
+    //         }
+    //     }
+    //     return new int[]{-1,-1};
+        //number index
+    Map<Integer,Integer> map = new HashMap<>();
+
+    for(int i=0;i<nums.length;i++) 
+    {
+        int pairValue = target - nums[i];
+        if(map.containsKey(pairValue))
         {
-            for(int j=i+1;j<nums.length;j++)
-            {
-                if(nums[i]+nums[j] == target)
-                { 
-                    return new int[]{i,j};
-                }       
-            }
+            int index1=i;
+            int index2=map.get(pairValue);
+
+           return new int[]{index1,index2};
         }
-       
-        return new int[]{-1,-1};
+        map.put(nums[i],i); //add number index
+    }
+    return new int[]{-1,-1};
+
+
     }
 }
+// 19 + (-10) = 9
+// nums = [2,11,8,3,7], target = 9
+
+// Map
+// Key Value
+// 2 0
+// 11 1
+// 8 2
+// 3 3
+// 7 4
 
 
+
+
+// boolean isPairPresent = map.containsKey(7) true / false
 
 
 
