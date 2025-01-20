234ba258d6d782f07879271c3d2d02ae6ea26d5a
SWARAJ D SHELAVALE <shelavaleswaraj1610@gmail.com>
2025-01-20 22:31:51 +0530

    Time: 15 ms (28.02%) | Memory: 44.7 MB (44.13%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -8,19 +8,22 @@ class Solution {
         {
             //For String1
             char ch = s.charAt(i);
-            mp.put(ch,mp.getOrDefault(ch,0)+1);
+            mp.put(ch,mp.getOrDefault(ch,0)+1);   
+        }
 
-             if(mp.get(ch)==0)
-                    mp.remove(ch);
-
+        for(int i=0;i<n;i++)
+        {
             //For String2
-            char ch2 = t.charAt(i);
-            mp.put(ch2,mp.getOrDefault(ch2,0)-1);
-
-            if(mp.get(ch2)==0)
-                    mp.remove(ch2);
+            char ch = t.charAt(i);
+            mp.put(ch,mp.getOrDefault(ch,0)-1);  
+            if(mp.get(ch) == 0)
+            {
+                mp.remove(ch);
+            } 
         }
         return mp.isEmpty();
 
     }
-}
+} 
+
+
